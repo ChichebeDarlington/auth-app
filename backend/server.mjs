0ba683dcde14d4126f650b0ea840a2c5651920e1
Madu Chichebe <chichebewebdev@gmail.com>
2024-10-30 01:14:35 +0100

    app ready to deploy

--- backend/server.mjs
+++ backend/server.mjs
@@ -4,6 +4,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoutes.mjs";
 import { connectDatabase } from "./database/storage.mjs";
+import path from "path";
 
 dotenv.config();
 
@@ -17,6 +18,16 @@ server.use(cors({ origin: "http://localhost:5173", credentials: true }));
 server.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 7000;
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  server.use(express.static(path.join(__dirname, "frontend/dist")));
+
+  server.get("*", (req, res) => {
+    return res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
+  });
+}
 server.listen(PORT, () => {
   connectDatabase(process.env.MONGO_URI);
   console.log(`Server running at port ${PORT}`);
